60cff5184cfbccda0893735f4b9be08668dd306b
Márcio Belo <mbelo@faeterj-rio.edu.br>
2019-09-16 18:35:04 -0300

    ajuste para o seguro saude usar o filter_input e formatação

--- seguro_saude/calculo.php
+++ seguro_saude/calculo.php
@@ -1,33 +1,26 @@
 <?php
 
   // obtem dados do formulário html
-  $nome = $_POST["nome"];
-  $codFaixaEtaria = $_POST["faixaEtaria"];
-
-  if( isset($_POST["doencaPrevia"]) )
-  {
-    $doencaPrevia = true;
-  } else {
-    $doencaPrevia = false;
-  }
-
-  function calculaPremio( $codFaixaEtaria, $doencaPrevia)
-  {
+  $nome = filter_input(INPUT_POST, "nome", FILTER_SANITIZE_STRING);
+  $codFaixaEtaria = filter_input(INPUT_POST, "faixaEtaria", FILTER_SANITIZE_NUMBER_INT);
+  $doencaPrevia = filter_input( INPUT_POST, "doencaPrevia") === "S" ? true : false;
+  
+  function calculaPremio( $codFaixaEtaria, $doencaPrevia) {
+	  
     $valorFaixaPuro = 200 * pow( 1.5, $codFaixaEtaria);
-
-    if( $doencaPrevia )
-    {
+    if( $doencaPrevia ) {
+		
       $valorFinal = $valorFaixaPuro * 1.3;
-    }
-    else
-    {
+    } else {
+		
       $valorFinal = $valorFaixaPuro; 
     }
     return $valorFinal;
   }
 
   $premio = calculaPremio( $codFaixaEtaria, $doencaPrevia);
-  ?>
+  $fmt = new NumberFormatter( 'pt_BR', NumberFormatter::CURRENCY );
+?>
 <!DOCTYPE html>
 <html>
   <head>
@@ -43,7 +36,7 @@
 
       <h2>Confira a cotação do seu seguro</h2>
 
-      <p>Sr(a).<?= htmlentities($nome); ?> seu seguro está cotado em <?= money_format('%=*^-14#8.2i',$premio) ?></p>
+      <p>Sr(a).<?= htmlentities($nome); ?> seu seguro está cotado em <?= $fmt->formatCurrency( $premio, 'BRL') ?></p>
 
     </div>
   </body>
